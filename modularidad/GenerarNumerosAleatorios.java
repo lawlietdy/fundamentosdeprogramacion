5c96bfbabf4616023d8128ef719aa46de6118c78
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-01-26 16:47:57 -0600

    Modularidad

--- modularidad/GenerarNumerosAleatorios.java
+++ modularidad/GenerarNumerosAleatorios.java
@@ -1,25 +1,34 @@
 import java.io.*;
+
 public class GenerarNumerosAleatorios {
-    public static void main(String[] args) throws IOException {
-	
-	int i=0, numeroAleatorio = 0, sumaNumeros = 0;
-	float promedioNumeros = 0f;
-	
-	InputStreamReader ent = new InputStreamReader(System.in);
-    BufferedReader al = new BufferedReader(ent);
-	
-	System.out.println("Generando numeros...");
-	
-	for (i=1; i<=20; i = i + 1){
-		numeroAleatorio = (int)(Math.random() * 10);
-		System.out.println(numeroAleatorio);
-		sumaNumeros = sumaNumeros + numeroAleatorio;
+	public static void main(String[] args) throws IOException {
+
+		int sumaNumeros;
+		float promedioNumeros = 0f;
+
+
+		System.out.println("Generando numeros...");
+
+		sumaNumeros = generadorAleatorios(20);
+		promedioNumeros = sumaNumeros / 20;
+
+		mostrarResultados(sumaNumeros, promedioNumeros);
+		
+	}
+
+	public static void mostrarResultados(int sumaNumeros, float promedioNumeros) {
+		System.out.println("La suma de los numeros es: " + sumaNumeros);
+		System.out.println("El promedio de los numeros es: " + promedioNumeros);
+	}
+
+	public static int generadorAleatorios(int total) {
+		int i = 0, numeroAleatorio = 0, sumaNumeros = 0;
+		for (i = 1; i <= 20; i = i + 1) {
+			numeroAleatorio = (int) (Math.random() * 10);
+			System.out.println(numeroAleatorio);
+			sumaNumeros = sumaNumeros + numeroAleatorio;
 		}
 		
-	promedioNumeros = sumaNumeros / 20;
-	
-	System.out.println("La suma de los numeros es: " + sumaNumeros);
-	System.out.println("El promedio de los numeros es: " + promedioNumeros);
-	
-	}	
+		return sumaNumeros;
+	}
 }
